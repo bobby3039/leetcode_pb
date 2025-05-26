5f68c8563f452504d098fd7caba8126e041caf59
bobby3039 <harsh102938@gmail.com>
2025-05-27 02:02:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -0,0 +1,44 @@
+#define le cout<<endl
+#define yes cout<<"YES"
+#define no cout<<"NO"
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define fl(i,n)   for(int i=0;i<(n);++i)
+#define fli(i,j,n)   for(int i=(j);i<=(n);++i)
+#define fld(i,j,n)   for(int i=(j);i>=(n);--i)
+#define all(x) begin(x), end(x)
+const int MOD = 1e9+7;
+class Solution {
+public:
+      
+    vector<int> largestDivisibleSubset(vector<int>& a) {
+        int n = a.size();
+        sort(all(a));
+        vi prev(n, -1);
+        vi len(n, 1);
+        fli(i,0,n-1){
+           fli(j,0,i-1){
+            if(a[i]%a[j]==0){prev[i]=j;}
+           }
+        }
+        fli(i,0,n-1){
+            if(prev[i]==-1)continue;
+            len[i]= len[prev[i]]+1;
+        }
+        int id =0;
+        int maxi = 0;
+        fli(i,0,n-1){
+            if(len[i]>maxi){
+                maxi = len[i];
+                id = i;
+            }
+        }
+        vector<int>ans;
+        while(id!=-1){
+            ans.push_back(a[id]);
+            id = prev[id];
+        }
+        return ans;
+    }
+};
