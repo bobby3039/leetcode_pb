0c834768f46e61bcbdb158ebc62044d26502238c
Harsh Kumar <harsh102938@gmail.com>
2025-08-09 22:48:11 +0530

    Time: 61 ms (31.47%), Space: 94 MB (39.99%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -4,7 +4,7 @@ public:
         sort(intervals.begin(), intervals.end());
         int a = -1e5, b = -1e5;
         int cnt = 0;
-        for(int i = 0 ; i < intervals.size(); i++){
+        for(int i = 0; i < intervals.size(); i++){
                int st = intervals[i][0];
                int en = intervals[i][1];
                if(st >= b){
@@ -14,9 +14,25 @@ public:
                else{
                 cnt++;
                }
-
-
         }
-        return cnt;
+
+        reverse(intervals.begin(), intervals.end());
+        a = 1e5, b = 1e5;
+        int cnt2 = 0;
+        for(int i=0; i<intervals.size(); i++){
+            int st = intervals[i][0];
+            int en = intervals[i][1];
+            if(en<=a){
+                a = st; b=en;
+                continue;
+            }
+            else{
+                cnt2++;
+            }
+        }
+
+
+
+        return cnt2;
     }
 };
